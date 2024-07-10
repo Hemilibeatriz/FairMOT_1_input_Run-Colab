483757e10866e23b4ee5920dbcaca970f6aa7c2e
hemilibeatriz <hemilibeatriz@gmail.com>
2024-07-10 07:55:01 -0300

    try to fix dlyfio down

--- src/lib/tracker/multitracker.py
+++ src/lib/tracker/multitracker.py
@@ -176,7 +176,7 @@ class JDETracker(object):
         else:
             opt.device = torch.device('cpu')
         print('Creating model...')
-        #self.model = create_model(opt.arch, opt.heads, opt.head_conv)
+        self.model = create_model(opt.arch, opt.heads, opt.head_conv)
         self.model = load_model(self.model, opt.load_model)
         self.model = self.model.to(opt.device)
         self.model.eval()
